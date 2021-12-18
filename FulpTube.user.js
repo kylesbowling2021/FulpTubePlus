b10c6be0ef1e76e0f06102d50df0ee61e812ab2f
MarkoczekTV <80894547+ClassicTube@users.noreply.github.com>
2021-12-18 15:30:06 +0100

    Update FulpTube.user.js

--- FulpTube.user.js
+++ FulpTube.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FulpTube+
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.fulptube.org/*
